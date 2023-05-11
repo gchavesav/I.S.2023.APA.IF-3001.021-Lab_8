eb9dfe04027dbfd4c6441c229dc889085ffdf335
LabB <afsfd@gmail.com>
2023-05-10 20:31:31 -0600

    -Solo falta counter

--- src/main/java/controller/CountingSortController.java
+++ src/main/java/controller/CountingSortController.java
@@ -20,21 +20,34 @@ public class CountingSortController
     @javafx.fxml.FXML
     private TableView notSortedTableView;
 
-    private Elementary elementaryArray;
+    private Elementary elementaryArray = new Elementary();
 
     private int a[] = new int [200];
 
+    private int counter[] = new int[200];
+
     @javafx.fxml.FXML
     public void initialize() {
+        util.Utility.fill(a,900);
         for (int i = 0; i < 200; i++) {
             final int colIndex = i;
             TableColumn<List<String>, String> column = new TableColumn<>(""+(i));
             column.setCellValueFactory(data->new SimpleStringProperty(data.getValue().get(colIndex)));
             notSortedTableView.getColumns().add(column);
-            /*counterTableView.getColumns().add(column);
-            countingSortedTableView.getColumns().add(column);*/
         }
-
+        notSortedTableView.setItems(getData());
+        for (int i = 0; i < 200; i++) {
+            final int colIndex = i;
+            TableColumn<List<String>, String> column = new TableColumn<>(""+(i));
+            column.setCellValueFactory(data->new SimpleStringProperty(data.getValue().get(colIndex)));
+            counterTableView.getColumns().add(column);
+        }
+        for (int i = 0; i < 200; i++) {
+            final int colIndex = i;
+            TableColumn<List<String>, String> column = new TableColumn<>(""+(i));
+            column.setCellValueFactory(data->new SimpleStringProperty(data.getValue().get(colIndex)));
+            countingSortedTableView.getColumns().add(column);
+        }
 
 
     }
@@ -49,17 +62,40 @@ public class CountingSortController
         return data;
     }
 
+    public ObservableList<List<String>> getDataSorted() {
+        ObservableList<List<String>> data = FXCollections.observableArrayList();
+        List<String> info = new ArrayList<>();
+        elementaryArray.countingSort(a);
+
+        for (int i = 0; i < counter.length; i++) {
+            info.add(String.valueOf(counter[i]));
+        }
+        data.add(info);
+        return data;
+    }
+
+    public ObservableList<List<String>> getDataCounter() {
+        ObservableList<List<String>> data = FXCollections.observableArrayList();
+        List<String> info = new ArrayList<>();
+        for (int i = 0; i < counter.length; i++) {
+            info.add(String.valueOf(counter[i]));
+        }
+        data.add(info);
+        return data;
+    }
+
 
     public void randomizeOnAction(ActionEvent actionEvent) {
-        util.Utility.fill(a,99);
+        util.Utility.fill(a,900);
         notSortedTableView.setItems(getData());
     }
 
     public void startOnAction(ActionEvent actionEvent) {
-        int []o = elementaryArray.getCounterArray();
-       elementaryArray.countingSort(a);
+        elementaryArray.countingSort(a);
 
-
+        countingSortedTableView.setItems(getData());
+        counter = elementaryArray.getCounterArray();
+        counterTableView.setItems(getDataCounter());
 
     }
 }
