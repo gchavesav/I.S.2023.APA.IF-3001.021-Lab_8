aced4f6b653acfe27048a45570a808ba1ac85196
Lani6 <chavespaula912@gmail.com>
2023-05-10 19:31:44 -0600

    Grupo1

---
+++ src/main/java/controller/mergeSortController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class mergeSortController {
+    @FXML
+    private TableView tableViewNoSortedArray;
+    @FXML
+    private TableView tableViewTempArray;
+    @FXML
+    private TableView tableViewRepeated;
+    @FXML
+    private TextField textFielLow;
+    @FXML
+    private TextField textFieldHigh;
+    @FXML
+    private TextField textFieldRecursiveCalls;
+
+    @FXML
+    public void initialize() {
+    }
+
+    @FXML
+    void onActionRandomize(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionStart(ActionEvent event) {
+
+    }
+}
