fed7fb9ad277daf0044e69e243e6fc976bfc77fe
Lani6 <chavespaula912@gmail.com>
2023-05-10 20:21:50 -0600

    Grupo1

--- src/main/java/controller/mergeSortController.java
+++ src/main/java/controller/mergeSortController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import domain.Complex;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -8,8 +9,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import util.Utility;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class mergeSortController {
@@ -26,10 +29,13 @@ public class mergeSortController {
     @javafx.fxml.FXML
     private TextField textFieldRecursiveCalls;
     int array[] = new int[200];
+    int arrayTemp[] = new int[200];
 
     @FXML
     public void initialize() {
-//agregamos las columnas al tableview
+        util.Utility.fill(array);
+        Complex complex = new Complex();
+        //agregamos las columnas al tableViewNoSortedArray
         for (int i = 0; i < 200; i++) {
             final int colIndex = i;
             TableColumn<List<String>, String> column = new TableColumn<>(String.valueOf(i));
@@ -37,11 +43,22 @@ public class mergeSortController {
             tableViewNoSortedArray.getColumns().add(column);
         }
         tableViewNoSortedArray.setItems(getData());
+        //ordenamos los datos
+
+        complex.mergeSort(array,arrayTemp,0,199);
+        System.out.println(Arrays.toString(array));
+
+        //agregamos las columnas al
+        for (int i = 0; i < 200; i++) {
+            final int colIndex = i;
+            TableColumn<List<String>, String> column = new TableColumn<>(String.valueOf(i));
+            column.setCellValueFactory(data->new SimpleStringProperty(data.getValue().get(colIndex)));
+            tableViewRepeated.getColumns().add(column);
+        }
+        tableViewRepeated.setItems(getData());
     }
 
     private ObservableList getData() {
-        this.array= new int[200];
-        util.Utility.fill(array);
         ObservableList<List<String>> data = FXCollections.observableArrayList();
         List<String> arrayList = new ArrayList<>();
         for (int i = 0; i < array.length; i++) {
