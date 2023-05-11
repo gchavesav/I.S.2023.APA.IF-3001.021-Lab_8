a75bcfb95bd5d402b57b9fa29384618e7c1d015c
Lani6 <chavespaula912@gmail.com>
2023-05-10 19:49:19 -0600

    Grupo1

--- src/main/java/controller/mergeSortController.java
+++ src/main/java/controller/mergeSortController.java
@@ -1,26 +1,54 @@
 package controller;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class mergeSortController {
-    @FXML
+    @javafx.fxml.FXML
     private TableView tableViewNoSortedArray;
-    @FXML
+    @javafx.fxml.FXML
     private TableView tableViewTempArray;
-    @FXML
+    @javafx.fxml.FXML
     private TableView tableViewRepeated;
-    @FXML
+    @javafx.fxml.FXML
     private TextField textFielLow;
-    @FXML
+    @javafx.fxml.FXML
     private TextField textFieldHigh;
-    @FXML
+    @javafx.fxml.FXML
     private TextField textFieldRecursiveCalls;
+    int array[] = new int[200];
 
     @FXML
     public void initialize() {
+//agregamos las columnas al tableview
+        for (int i = 0; i < 200; i++) {
+            final int colIndex = i;
+            TableColumn<List<String>, String> column = new TableColumn<>(String.valueOf(i));
+            column.setCellValueFactory(data->new SimpleStringProperty(data.getValue().get(colIndex)));
+            tableViewNoSortedArray.getColumns().add(column);
+        }
+        tableViewNoSortedArray.setItems(getData());
+    }
+
+    private ObservableList getData() {
+        this.array= new int[200];
+        util.Utility.fill(array);
+        ObservableList<List<String>> data = FXCollections.observableArrayList();
+        List<String> arrayList = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            arrayList.add(String.valueOf(array[i]));
+        }
+        data.add(arrayList);
+        return data;
     }
 
     @FXML
@@ -30,6 +58,7 @@ public class mergeSortController {
 
     @FXML
     void onActionStart(ActionEvent event) {
-
+        util.Utility.fill(array);
+        tableViewNoSortedArray.setItems(getData());
     }
 }
