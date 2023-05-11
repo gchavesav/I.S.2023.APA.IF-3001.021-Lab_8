80b80572ef5a64840e2421113d10307b92a74feb
BlancaSC13 <blanca.segura@ucr.ac.cr>
2023-05-10 19:34:14 -0600

    quickSort.fxml

--- src/main/java/controller/QuickSortController.java
+++ src/main/java/controller/QuickSortController.java
@@ -1,9 +1,16 @@
 package controller;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class QuickSortController
 {
@@ -19,12 +26,35 @@ public class QuickSortController
     private TextField txtHigh;
     @javafx.fxml.FXML
     private TableView tblViewSorted;
+    private int array[];
 
     @javafx.fxml.FXML
     public void initialize() {
+//agregamos las columnas al tableview
+        for (int i = 0; i < 200; i++) {
+            final int colIndex = i;
+            TableColumn<List<String>, String> column = new TableColumn<>(String.valueOf(i));
+            column.setCellValueFactory(data->new SimpleStringProperty(data.getValue().get(colIndex)));
+            tblViewNoSorted.getColumns().add(column);
+        }
+        tblViewNoSorted.setItems(getData());
     }
+    private ObservableList getData() {
+        this.array= new int[200];
+        util.Utility.fill(array);
+        ObservableList<List<String>> data = FXCollections.observableArrayList();
+        List<String> arrayList = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            arrayList.add(String.valueOf(array[i]));
+        }
+        data.add(arrayList);
+        return data;
+    }
+
     @FXML
     void btnRandomizeOnAction(ActionEvent event) {
+        util.Utility.fill(array);
+        tblViewNoSorted.setItems(getData());
 
     }
 
