7247dca90a897d35820cbfba06ff3bf15df63644
GaCha22 <gchavesrojas@gmail.com>
2023-05-10 18:57:28 -0600

    Cambios en el controller

--- src/main/java/controller/ShellSortController.java
+++ src/main/java/controller/ShellSortController.java
@@ -1,7 +1,43 @@
 package controller;
 
-public class ShellSortController
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ShellSortController {
+
+    @FXML
+    private TableView<?> tblNoSortedArray;
+
+    @FXML
+    private TableView<?> tblSortedArray;
+
+    @FXML
+    private TextField txtGap1;
+
+    @FXML
+    private TextField txtGap2;
+
+    @FXML
+    private TextField txtGap3;
+
+    @FXML
+    private TextField txtGap4;
+
+    @FXML
+    void initialize(){
+
+    }
+
+    @FXML
+    void onActionRandomize(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionStart(ActionEvent event) {
+
+    }
+
+}
